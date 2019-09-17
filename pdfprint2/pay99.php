e6b320853bbe659992d159bc1f3a5aa348e89eb9
Marlon <mtc@cubeworks.com.ph>
2019-09-17 18:40:49 +0800

    Update Paylsip

--- pdfprint2/pay99.php
+++ pdfprint2/pay99.php
@@ -12,7 +12,7 @@ $query = mysqli_query($con,"select * from payroll3399
 	where PtabID = '".$_GET['ps1']."' AND Password = '".$_GET['pw2']."'");
 	
 
-$pdf = new FPDF('L','mm',array(270,210));
+$pdf = new FPDF('L','mm',array(210,279));
 
 $invoice = mysqli_fetch_array($query );
 $pdf->AddPage();
--- pdfprint2/payslip.php
+++ pdfprint2/payslip.php
@@ -12,7 +12,7 @@ $query = mysqli_query($con,"select * from payrollamt33
 	where PtabID = '".$_GET['ps1']."'");
 	
 
-$pdf = new FPDF('L','mm',array(270,210));
+$pdf = new FPDF('L','mm',array(210,279));
 
 $invoice = mysqli_fetch_array($query );
 $pdf->AddPage();
